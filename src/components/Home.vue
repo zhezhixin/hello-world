655eadb50f85d1b3c33965be5522ea0c546cf3ba
zhezhixin <zhezhixin@sina.cn>
2020-08-08 16:19:44 +0800

    fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,8 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <p>
     <router-link to="/helloworld">go to helloworld</router-link> |  
-    <router-link to="/todo">go to todolist</router-link>
-    <router-link to="others">go to others</router-link>
+    <router-link to="/todo">go to todolist</router-link> | 
+    <router-link to="/others">go to others</router-link>
     </p>
   </div>
 </template>
