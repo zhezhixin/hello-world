a58f762576f08ddd38878339ca278ffd69168bc0
zhezhixin <zhezhixin@sina.cn>
2020-08-12 11:42:05 +0800

    add echarts example

---
+++ src/ech/Index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="zz-echart">
+    <div id='myChart' :style="{width: '300px',height:'300px'}"></div>
+    </div>
+</template>
+<script>
+import ECharts from 'echarts'
+
+import 'echarts/lib/echart/line'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/polar'
+
+export default {
+    methods: {
+        drawLine(){
+             let myChart = ECharts.init(document.getElementById('myChart'))
+             myChart.setOption({
+                 title: {text:'use echarts'},
+                 tooltip: {},
+                 xAxis: {
+                     data:['a','b','c','d','e','f']
+                 },
+                 yAxis: {},
+                 series: [{
+                     name: 'sell',
+                     type: 'bar',
+                     data:['5,20,36,10,10,20']
+                 }]
+             });
+        }
+    },
+    mounted () {
+        this.drawLine();
+    }
+}
+</script>
---
+++ src/ech/ZzEchart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="zz-echart">
+
+    </div>
+</template>
+<script>
+import ECharts from 'vue-echarts'
+
+import 'echarts/lib/echart/line'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/polar'
+
+import 'echarts-gl'
+export default {
+
+}
+</script>
