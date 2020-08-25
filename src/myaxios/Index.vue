9819ecc6d3aadbc4ca6c20f4e184b82e70414f48
zhezhixin <zhezhixin@sina.cn>
2020-08-25 17:38:20 +0800

    add async and 本地后端django

--- src/myaxios/Index.vue
+++ src/myaxios/Index.vue
@@ -3,7 +3,7 @@
         <div class="box">
             <div class="box2">
                 <h1>Bitcoin Price Index</h1>
-                
+
                 <section v-if="errored">
                     <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
                 </section>
@@ -25,6 +25,18 @@
 
                 </section>
             </div>
+           <div>{{text}}</div>
+
+           <button @click="clickHandler">点击</button>
+           <div
+                class="currency"
+                v-for="(content,index) in msg"
+                :key="index"
+           >
+                <span class="lighten">{{content.id}}</span>
+                <span class="lighten">{{content.name}}</span>
+           </div>
+
         </div>
     </div>
 </template>
@@ -33,9 +45,12 @@ import Axios from "axios"
 export default {
     data () {
         return {
+            datainfo: null,
             info: null,
             loading:true,
-            errored:false
+            errored:false,
+            text: null,
+            msg:null
         }
     },
     filters: {
@@ -43,18 +58,52 @@ export default {
             return val.toFixed(2)
         }
     },
+    methods:{
+        async hello (){
+            this.text = "异步请求"
+            return "后执行异步请求"
+        },
+
+        //https://www.cnblogs.com/Eeyhan/p/10579239.html
+        clickHandler () {
+            Axios
+                .get("http://127.0.0.1:8000/data/")
+                .then(response => {
+                    console.log(response.data)
+                    this.msg = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    },
     mounted () {
+        //下面是axios获取后端数据
         Axios
             .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-            .then(Response =>{
-                this.info = Response.data.bpi
+            .then(response =>{
+                //console.log(response)
+                //console.log(response.data.bpi)
+                this.datainfo = response
+                this.info = response.data.bpi
             })
             .catch(error => {
                 console.log(error)
                 this.errored = true
             })
-            .finally(() => this.loading = false)
+            .finally(() => this.loading = false);
+        
+        //下面是异步执行async async异步函数返回的是一个promise对象，
+        //如果要获取到promise返回值，我们就应该使用.then方法。
+        this.hello().then(result => { //不要忘记this.hello() ，不是hello()
+            console.log(result);
+        })
+        console.log(this.hello());
+        console.log('先执行');
+
+        
     }
+    
 }
 </script>
 <style>
